0cbd7b8b35adf21b5f19dc00b8d3b58d6d35d8da
Matt5246 <116891485+Matt5246@users.noreply.github.com>
2024-10-10 00:03:17 +0200

    progress page added

---
+++ my-app/src/app/home/progress/page.tsx
@@ -0,0 +1,10 @@
+import Chart from './Chart'
+import HistoryList from './HistoryList';
+export default function Home() {
+    return (
+        <>
+            <Chart />
+            <HistoryList />
+        </>
+    );
+}
